ecb36c19af34c342855800da81d57bc35a5052e0
prettybydefault <adetoladediran@gmail.com>
2024-07-13 19:22:15 +0100

    Add files via upload

---
+++ JAVA programming/task.java
@@ -0,0 +1,12 @@
+package JAVA programming;
+import java.util.Scanner;
+public class task {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter any number between 1 to 1000:");
+            int n1 = scanner.nextInt();
+            int n1n1= n1 * n1;
+            System.out.println("square of number:"+n1n1);
+
+        }
+    }
